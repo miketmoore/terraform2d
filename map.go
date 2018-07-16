ecc4d61b960bd9115323cdb2c703b4d3dea8552f
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-16 08:36:09 -0400

    connected rooms

--- map.go
+++ map.go
@@ -2,3 +2,11 @@ package terraform2d
 
 // RoomID is a room ID
 type RoomID int
+
+// ConnectedRooms is used to configure adjacent rooms
+type ConnectedRooms struct {
+	Top    RoomID
+	Right  RoomID
+	Bottom RoomID
+	Left   RoomID
+}
